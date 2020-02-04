b452633160c6bde89ba5ee3748e27ada03dc4078
Auriane Cordier <auriane.cordier@free.fr>
2020-02-04 10:16:31 +0100

    Nouveau test pour les boutons car meilleure compréhension

--- source/image_recognition/Level.py
+++ source/image_recognition/Level.py
@@ -22,36 +22,29 @@
         self.draw_handler = DrawingHandler(self.tex_handler, q_activate, liquid_im)
 
         # Create a class that will extract "bricks" from image
-        self.brick_recognition = BrickRecognition(liquid_im)
+        # self.brick_recognition = BrickRecognition(liquid_im)
 
         # Create button Handler and start them for image detection
-        self.buttonStart, self.buttonReset = None, None
+        self.buttonCorrosion, self.buttonMecanique, self.buttonThermique = None, None, None
         self.init_start_buttons()
 
     def init_start_buttons(self):
-        self.buttonStart = HandButton(0, None, 3, Conf.hand_area_3, Conf.hand_threshold_1)
-        self.buttonReset = HandButton(1, None, 1, Conf.hand_area_4, Conf.hand_threshold_2)
-        self.buttonStart.daemon = True
-        self.buttonReset.daemon = True
-        self.buttonStart.start()
-        self.buttonReset.start()
-        self.buttonReset.title = "Corrosion"
+        self.buttonCorrosion = HandButton(0, None, 3, Conf.hand_area_3, Conf.hand_threshold_1)
+        self.buttonMecanique = HandButton(1, None, 1, Conf.hand_area_4, Conf.hand_threshold_2)
+        self.buttonThermique = HandButton(1, None, 1, Conf.hand_area_5, Conf.hand_threshold_2)
 
-        self.buttonStart = HandButton(0, None, 3, Conf.hand_area_4, Conf.hand_threshold_1)
-        self.buttonReset = HandButton(1, None, 1, Conf.hand_area_5, Conf.hand_threshold_2)
-        self.buttonStart.daemon = True
-        self.buttonReset.daemon = True
-        self.buttonStart.start()
-        self.buttonReset.start()
-        self.buttonReset.title = "Thermique"
+        self.buttonCorrosion.daemon = True
+        self.buttonMecanique.daemon = True
+        self.buttonThermique.daemon = True
 
-        self.buttonStart = HandButton(0, None, 3, Conf.hand_area_1, Conf.hand_threshold_1)
-        self.buttonReset = HandButton(1, None, 1, Conf.hand_area_2, Conf.hand_threshold_2)
-        self.buttonStart.daemon = True
-        self.buttonReset.daemon = True
-        self.buttonStart.start()
-        self.buttonReset.start()
-        self.buttonReset.title = "Mécanique"
+        self.buttonCorrosion.start()
+        self.buttonMecanique.start()
+        self.buttonThermique.start()
+
+        self.buttonCorrosion.title = "Corrosion"
+        self.buttonMecanique.title = "Mécanique"
+        self.buttonThermique.title = "Thermique"
+
 
     def reset(self):
         """ Reset program state"""
@@ -62,58 +55,75 @@
         self.triggered_start, self.triggered_reset = False, False
         self.wait_time = 1
 
+        self.buttonMecanique.is_triggered, self.buttonMecanique.is_waiting = False, False
+        Glob.brick_array.reset()
+
+    def render(self) -> None:
+            """ render the scene with OpenGL"""
+
+        if Glob.frame is not None:
+                # Set button text
+            if Glob.mode == 0:
+                self.draw_handler.q = 0.0
+                self.number = -1
+                self.buttonStart.title = "VALIDER"
+            elif self.buttonStart.is_ready():
+                self.buttonStart.title = "CONTINUER"
+            else:
+                self.buttonStart.title = "%i" % self.buttonStart.remaining_time
+
+            if self.number == -1:
+                self.buttonStart.wait_time = 1
+            else:
+                self.buttonStart.wait_time = 10
+
+            # Set liquid state from buttons
+            poor_liquid = False
+            if self.buttonStart.is_triggered and self.number >= 0:
+                poor_liquid = True
+
+            # step 1 : draw background
+            self.draw_handler.draw_frame(Glob.frame)
+
+            y0, x0 = Conf.cam_area[0]
+            yf, xf = Conf.cam_area[1]
+            # step 2 : draw molten steel
+            self.draw_handler.draw_molten_steel(x0, y0, xf - x0, yf - y0, 1, 1, 1, poor_liquid)
+
+            # step 3 : draw user interface
+            self.draw_handler.draw_ui(start_button=self.buttonStart, number=max(0, self.number))
+
+            # step 4 : draw buttons interfaces, reset button depends on the mode
+            self.buttonStart.draw()
+            if Glob.mode == 1 and self.buttonStart.is_ready():
+                if self.new:
+                    self.number += 1
+                    self.draw_handler.q += Glob.brick_array.current_steel_volume()
+                    self.new = False
+                self.buttonReset.unpause()
+                self.buttonReset.draw()
+            else:
+                self.buttonReset.pause()
+                self.new = True
 
     def check_buttons(self) -> None:
         """ Update button image and read button state """
 
         # Set image to the newest one
-        self.buttonStart.image = self.cam.image_raw
-        self.buttonReset.image = self.cam.image_raw
+        self.buttonCorrosion.image = self.cam.image_raw
+        self.buttonMecanique.image = self.cam.image_raw
+        self.buttonThermique.image = self.cam.image_raw
 
         # Change mode with button state
         if Glob.mode == 0 and Glob.brick_array is not None:
             self.triggered_number = self.buttonStart.number
 
-            if self.buttonStart.is_triggered:
-                Glob.mode = 1
+            if self.buttonCorrosion.is_triggered:
+                Glob.mode = 3
                 Glob.brick_array.init_heat_eq()
             pass
 
-            if self.buttonReset.is_triggered:
-                Glob.mode = 0
+            if self.buttonMecanique.is_triggered:
+                Glob.mode = 3
                 return
             pass
-
-    def detect_brick(self):
-        """ Execute brick detection tools """
-        frame = self.cam.image_raw
-        if frame is not None:
-            image_1, image_2 = self.brick_recognition.update_bricks(frame.copy())
-
-            # if we are calibrating print brick map on the screen (upper left corner)
-            if image_1 is not None:
-                if Glob.debug:
-                    texture = cv2.resize(image_1, (Conf.width, Conf.height))
-                    mask = np.zeros(texture.shape, dtype=np.uint8)
-                    grid_color = (0, 255, 0, 255)
-                    start_color = (0, 0, 255, 255)
-                    reset_color = (255, 0, 0, 255)
-                    mask[Conf.cam_area[0][0]:Conf.cam_area[1][0],
-                    Conf.cam_area[0][1]:Conf.cam_area[1][1]] = grid_color
-                    mask[Conf.hand_area_1[0][0]:Conf.hand_area_1[1][0],
-                    Conf.hand_area_1[0][1]:Conf.hand_area_1[1][1]] = start_color
-                    mask[Conf.hand_area_2[0][0]:Conf.hand_area_2[1][0],
-                    Conf.hand_area_2[0][1]:Conf.hand_area_2[1][1]] = reset_color
-
-                    texture = cv2.addWeighted(texture, 0.75, mask, 0.25, 0)
-                    # texture = np.mean([texture, mask], axis=0, dtype=np.uint8)
-                    self.tex_handler.bind_texture(0, cv2.flip(texture, 0), Conf.width, Conf.height)
-
-                    texture = cv2.resize(image_2, (Conf.width, Conf.height))
-                    self.tex_handler.bind_texture(5, cv2.flip(texture, 0), Conf.width, Conf.height)
-                Glob.frame = image_1
-
-    def lost_screen(self):
-        """ Draw a message on the screen """
-        self.draw_handler.draw_text_screen()
-
