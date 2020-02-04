f069f1e85e96641c6a6fe914083895d4ee8176e5
Alois <alois600@gmail.com>
2020-02-04 16:13:46 +0100

    level.py --> corrections/ ajouts

--- source/image_recognition/Level.py
+++ source/image_recognition/Level.py
@@ -1,7 +1,7 @@
 
     def __init__(self, cam, width: int, height: int, q_activate: SimpleQueue, liquid_im, liquid_grid) -> None:
         super().__init__(cam)
-
+        #
         # Create a texture handler with 6 different textures
         self.tex_handler = TextureHandler(6)
 
