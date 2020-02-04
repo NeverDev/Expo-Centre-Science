1b9cb62e84e1fc12009e1cbabfe944aff83d1028
Alois <alois600@gmail.com>
2020-02-04 16:58:30 +0100

    correction du git

--- source/image_recognition/Level.py
+++ source/image_recognition/Level.py
@@ -1,6 +1,6 @@
     def __init__(self, cam, width: int, height: int, q_activate: SimpleQueue, liquid_im, liquid_grid) -> None:
         super().__init__(cam)
-        #
+
         # Create a texture handler with 6 different textures
         self.tex_handler = TextureHandler(6)
 
@@ -41,55 +41,6 @@
             self.buttonThermique.draw()
             self.buttonValider.draw()
 
-<<<<<<< HEAD
-        if Glob.frame is not None:
-                # Set button text
-            if Glob.mode == 0:
-                self.draw_handler.q = 0.0
-                self.number = -1
-                self.buttonCorrosion.title = "VALIDER"
-            elif self.buttonCorrosion.is_ready():
-                self.buttonCorrosion.title = "CONTINUER"
-            else:
-                self.buttonCorrosion.title = "%i" % self.buttonStart.remaining_time
-
-            if self.number == -1:
-                self.buttonCorrosion.wait_time = 1
-            else:
-                self.buttonCorrosion.wait_time = 10
-
-            # Set liquid state from buttons
-            poor_liquid = False
-            if self.buttonCorrosion.is_triggered and self.number >= 0:
-                poor_liquid = True
-
-            # step 1 : draw background
-            self.draw_handler.draw_frame(Glob.frame)
-
-            y0, x0 = Conf.cam_area[0]
-            yf, xf = Conf.cam_area[1]
-            # step 2 : draw molten steel
-            self.draw_handler.draw_molten_steel(x0, y0, xf - x0, yf - y0, 1, 1, 1, poor_liquid)
-
-            # step 3 : draw user interface
-            self.draw_handler.draw_ui(start_button=self.buttonCorrosion, number=max(0, self.number))
-
-            # step 4 : draw buttons interfaces, reset button depends on the mode
-            self.buttonCorrosion.draw()
-            if Glob.mode == 1 and self.buttonCorrosion.is_ready():
-                if self.new:
-                    self.number += 1
-                    self.draw_handler.q += Glob.brick_array.current_steel_volume()
-                    self.new = False
-                self.buttonMecanique.unpause()
-                self.buttonMecanique.draw()
-            else:
-                self.buttonMecanique.pause()
-                self.new = True
-=======
-            # + interface draw if needed
-
->>>>>>> master
 
     def check_buttons(self) -> None:
         """ Update button image and read button state """
@@ -100,21 +51,6 @@
         self.buttonThermique.image = self.cam.image_raw
         self.buttonValider.image = self.cam.image_raw
 
-<<<<<<< HEAD
-        # Change mode with button state
-        if Glob.mode == 0 and Glob.brick_array is not None:
-            self.triggered_number = self.buttonCorrosion.number
-
-            if self.buttonCorrosion.is_triggered:
-                Glob.mode = 3
-                Glob.brick_array.init_heat_eq()
-            pass
-
-            if self.buttonMecanique.is_triggered:
-                Glob.mode = 3
-                return
-            pass
-=======
         if self.buttonCorrosion.is_triggered and not "Corrosion" in Glob.physics:
             Glob.physics.append("Corrosion")
         if self.buttonMecanique.is_triggered and not "Mecanique" in Glob.physics:
@@ -123,8 +59,4 @@
             Glob.physics.append("Thermique")
         if self.buttonValider.is_triggered and len(Glob.physics) > 0:
             # change to game
->>>>>>> master
 
-
-
-Test.pour.voir.si.tu.vois.la.dif
